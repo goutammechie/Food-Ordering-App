2ef1e931d8b531d917d324ffa471011e5cabdb34
goutammechie <2019meb1258@iitrpr.ac.in>
2022-09-29 11:58:49 +0530

    checking for product name and price

--- src/pages/Menu/index.jsx
+++ src/pages/Menu/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch,useSelector } from "react-redux";
 import { fetchProducts, selectAllProducts } from '../../stores/cart/menu/productsSlice';
+import ProductDetailCard from '../../components/productDetailCard';
 
 
 const Menu =()=> {
@@ -8,20 +9,21 @@ const Menu =()=> {
     const products = useSelector(selectAllProducts);
     console.log(products)
     useEffect(()=>{
-        dispatch(fetchProducts())
-}, [])
+        dispatch(fetchProducts())}, [])
     return (
-        <div>
+        <div  className='bg-white'>
             {
                 
                 products? products.status === 'fulfilled'? <div className='menu-wrapper'>
                     {
-                        products.products && products.products.map((product, index)=> {
-                            console.log(index, product)
-                            return (
-                               <div key = {index} className='text-white'>{product.name.name}</div>
-                            )
-                        })
+                       
+                            products.products && products.products[0].products.map((product, index)=> {
+                                // console.log(index, product)
+                                return (
+                                   <ProductDetailCard key={index} product={product} />
+                                )
+                            })
+                            
                     }
                 </div>:
                 <div>LOADING......</div>:<div>LOADING......</div>
