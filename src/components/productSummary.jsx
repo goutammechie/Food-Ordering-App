d7c8c9e39d62bd8311c6c025fed760dc1cc79a1a
goutammechie <2019meb1258@iitrpr.ac.in>
2022-10-02 16:31:34 +0530

    installing stripe

--- src/components/productSummary.jsx
+++ src/components/productSummary.jsx
@@ -1,5 +1,17 @@
-export const ProductSummary=()=>{
+import { useSelector } from "react-redux";
+import { cartProducts } from "../stores/cart/cartSlice";
+import { ProductsSummaryCard } from "./ProductSummaryCard";
+
+export const ProductSummary = () => {
+    const cart = useSelector(cartProducts);
+
     return (
-        <></>
+        <div className="flex flex-col">
+            { cart && cart?.map((product, index) => {
+                return (
+                    <ProductsSummaryCard product={product} key={index} />
+                )
+            })}
+        </div>
     )
 }
