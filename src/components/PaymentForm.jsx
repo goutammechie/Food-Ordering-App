f2a96f5505429484118db8bfe19d4f32cc6d4a92
goutammechie <2019meb1258@iitrpr.ac.in>
2022-10-12 15:51:52 +0530

    upadated card_element in PaymentForm.jsx

--- src/components/PaymentForm.jsx
+++ src/components/PaymentForm.jsx
@@ -16,7 +16,7 @@ const PaymentForm = () =>{
         <form className="md:-2/3 md:mx-auto px-2 pt-1" id="payment-form">
             <label htmlfor="card-element" className="pt-4 text-2xl md:text-center"> Please enter you card details </label>
             <div>
-                <cardElement id="card-element"/>
+                <CardElement id="card-element"/>
             </div>
         </form>
     )
